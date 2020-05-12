fdfc24a58231947c62ff6b90a092b6ab4a02204a
JoeyOhman <joeyoh@kth.se>
2020-05-12 02:18:08 +0200

    k-layer nets

--- Assignment3/A3.py
+++ Assignment3/A3.py
@@ -17,7 +17,7 @@ N_S = 2 * np.floor(N / BATCH_SIZE)
 
 
 def unpackData():
-    fileNames = ["data_batch_1"]  # , "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
+    fileNames = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
 
     trainX = []
     trainY = []
@@ -82,16 +82,15 @@ def computeAccuracy(X, Y, W, b):
     return numCorrect / numDataPoints
 
 
+# Xavier normalization
 def initializeParams(dimensions):
     W = []
     b = []
-    prevDim = d
-    for i in range(len(dimensions)):
+    for i in range(1, len(dimensions)):
+        prevDim = dimensions[i - 1]
         newW = np.random.normal(0, 1 / np.sqrt(prevDim), (dimensions[i], prevDim))
         W.append(newW)
         b.append(np.zeros((dimensions[i], 1)))
-
-        prevDim = dimensions[i]
 
     return W, b
 
@@ -103,14 +102,15 @@ def computeGradients(X, Y, W, lam):
 
     G = -(Y - X[-1])
 
-    for k in range(len(W)-1, 1):
-        dLdW = numDataPointsInv * (G @ X[k-1].T)
+    # print(len(W) - 1)
+    for k in range(len(W) - 1, 0, -1):
+        dLdW = numDataPointsInv * (G @ X[k].T)
         gW = dLdW + 2 * lam * W[k]
         gB = np.expand_dims(np.mean(G, axis=1), 1)
         gradW.append(gW)
         gradB.append(gB)
 
-        indMat = np.where(X[k-1] > 0, 1, 0)
+        indMat = np.where(X[k] > 0, 1, 0)
         G = (W[k].T @ G) * indMat
 
     dLdW = numDataPointsInv * (G @ X[0].T)
@@ -119,4 +119,160 @@ def computeGradients(X, Y, W, lam):
     gradW.append(gW)
     gradB.append(gB)
 
-    return gradW.reverse(), gradB.reverse()  # TODO: REVERSE! does it work like this? is this what I want?
+    return gradW[::-1], gradB[::-1]  # Reverse
+
+
+# TODO: Iterate over k
+def gradDiff(X, Y, W, lam, b, numLayers):
+    H = forward(X, W, b)
+    gradW, gradB = computeGradients(H, Y, W, lam)
+
+    numGradW, numGradB = funcs.computeGradsNum(X, Y, W, b, lam, 1e-5, computeCost, numLayers)
+
+    maxDiffW = 0
+    maxDiffB = 0
+    for k in range(numLayers):
+        diffW = np.max(np.abs(gradW[k] - numGradW[k]))
+        diffB = np.max(np.abs(gradB[k] - numGradB[k]))
+        maxDiffW = max(diffW, maxDiffW)
+        maxDiffB = max(diffB, maxDiffB)
+
+    print(maxDiffW)
+    print(maxDiffB)
+    if maxDiffW < 1e-6:
+        print("Gradient W correct!")
+    else:
+        print("Gradient W incorrect!")
+
+    if maxDiffB < 1e-6:
+        print("Gradient b correct!")
+    else:
+        print("Gradient b incorrect!")
+
+
+def plotLearningCurves(history):
+    labels = ["Training", "Validation"]
+    for k in [0, 2, 4]:
+        for i, h in enumerate(history[k:k+2]):
+            plt.plot(h, label=labels[i])
+
+        plt.legend()
+        plt.xlabel("Epoch")
+        plt.show()
+
+
+def evaluateModel(xTrain, yTrain, xTest, yTest, W, b, lam):
+    # These could be optimized by sharing forward props
+    costTrain = computeCost(xTrain, yTrain, W, b, lam)
+    costTest = computeCost(xTest, yTest, W, b, lam)
+    lossTrain = computeCost(xTrain, yTrain, W, b, 0)
+    lossTest = computeCost(xTest, yTest, W, b, 0)
+    accTrain = computeAccuracy(xTrain, yTrain, W, b)
+    accTest = computeAccuracy(xTest, yTest, W, b)
+
+    # print("Training Loss: {}".format(lossTrain))
+    # print("Test Loss: {}".format(lossTest))
+    return costTrain, costTest, lossTrain, lossTest, accTrain, accTest
+
+
+def getLearningRate(t):
+    diff = ETA_MAX - ETA_MIN
+    lCyc = int(t / (2 * N_S))
+
+    if 2 * lCyc * N_S <= t <= (2 * lCyc + 1) * N_S:
+        return ETA_MIN + (t - 2 * lCyc * N_S) * diff / N_S
+    else:
+        return ETA_MAX - (t - (2 * lCyc + 1) * N_S) * diff / N_S
+
+
+def miniBatchGD(X, Y, GDParams, W, b, lam, xVal, yVal):
+    nBatch, lr, nEpochs = GDParams
+    numDataPoints = X.shape[1]
+    costsTrain, costsTest, lossesTrain, lossesTest, accsTrain, accsTest = [], [], [], [], [], []
+    t = 0
+
+    for epoch in range(nEpochs):
+
+        print("Epoch", epoch + 1)
+        costTrain, costTest, lossTrain, lossTest, accTrain, accTest = evaluateModel(X, Y, xVal, yVal, W, b, lam)
+        costsTrain.append(costTrain)
+        costsTest.append(costTest)
+        lossesTrain.append(lossTrain)
+        lossesTest.append(lossTest)
+        accsTrain.append(accTrain)
+        accsTest.append(accTest)
+
+        # Randomize
+        perm = np.random.permutation(numDataPoints)
+        X = X[:, perm]
+        Y = Y[:, perm]
+
+        # Iterate batches
+        batchIndices = np.arange(0, numDataPoints, nBatch)
+        for startIdx in batchIndices:
+            lr = getLearningRate(t)
+            t += 1
+            endIdx = min(startIdx + nBatch, numDataPoints)
+            xBatch = X[:, startIdx:endIdx]
+            yBatch = Y[:, startIdx:endIdx]
+            hBatch = forward(xBatch, W, b)  # forward pass
+            gW, gB = computeGradients(hBatch, yBatch, W, lam)  # backward pass
+            for k in range(len(W)):
+                W[k] -= lr * gW[k]
+                b[k] -= lr * gB[k]
+
+    # Last update to history
+    costTrain, costTest, lossTrain, lossTest, accTrain, accTest = evaluateModel(X, Y, xVal, yVal, W, b, lam)
+    costsTrain.append(costTrain)
+    costsTest.append(costTest)
+    lossesTrain.append(lossTrain)
+    lossesTest.append(lossTest)
+    accsTrain.append(accTrain)
+    accsTest.append(accTest)
+
+    history = costsTrain, costsTest, lossesTrain, lossesTest, accsTrain, accsTest
+    return W, b, history
+
+
+def train():
+    trainX, trainY, valX, valY, testX, testY = unpackData()
+    X = trainX[:, :]
+    Y = trainY[:, :]
+
+    dims = [d, 50, 30, 20, 20, 10, 10, 10, 10, K]
+    W, b = initializeParams(dims)
+    numCycles = 5
+    numEpochs = int(numCycles * int(N_S * 2 / 450))
+    print("NumEpochs:", numEpochs)
+    lam = 0.00137
+    lr = 0.001
+
+    # gradDiff(X, Y, W, lam, b)
+    params = (BATCH_SIZE, lr, numEpochs)
+    W, b, history = miniBatchGD(X, Y, params, W, b, lam, valX, valY)
+
+    print("Final Train Accuracy:", computeAccuracy(X, Y, W, b))
+    print("Final Test Accuracy:", computeAccuracy(testX, testY, W, b))
+    plotLearningCurves(history)
+
+
+def run():
+    trainX, trainY, valX, valY, testX, testY = unpackData()
+    # X = trainX[:10, :1]
+    X = trainX
+    # Y = trainY[:, :1]
+    Y = trainY
+    dims = [d, m, K]
+
+    W, b = initializeParams(dims)
+    # W[0] = W[0][:, :10]
+    # print(W[0].shape)
+    # print(X.shape)
+    lam = 0.01
+    # [print(w.shape) for w in W]
+    numLayers = len(W)
+    gradDiff(X, Y, W, lam, b, numLayers)
+
+
+if __name__ == '__main__':
+    train()
